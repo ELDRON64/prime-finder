fc939d9ea05eccc2dc2c1104ddd5a04904d7508b
ELDRON64 <rethernet@gmail.com>
2023-01-08 22:10:31 +0100

    added documentation

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # prime finder
 
-This program uses an impementation of the sieve of eratosthenes for computing prime numbers
+This program uses an implementation of the sieve of eratosthenes for computing prime numbers
 
-the tree inital defines define the dimension of the chunk and the number of chaunk that will be considered,
-you can also modify the initial point
+## Important notes
+
+- Defines  
+  Here are defined all the parameters necessary to operate the program
+  - chunk_initial_delta: the number where you want to start
+  - chunk_size: the allocated for a single chunk expressed in bytes
+  - chunk_to_elaborate: number of chunks to elaborate
+  - out_path: the output path
+
+- Print  
+  The program prints the numbers in binary form.  
+  Every number is expressed as a std::size_t number ( 8 byte length )
+
+## release notes
+
+Due to an error the primes between chunks can be skipped
--- finder.cpp
+++ finder.cpp
@@ -1,11 +1,16 @@
 #include <math.h>
 #include <fstream>
 #include <iostream>
+
 #define chunk_initial_delta 0
-#define chunk_size 1073741824
+#define chunk_size 2048  // 1073741824
 #define chunk_to_elaborate 2
+#define out_path "primes.txt"
+
 int main() {
-	std::ofstream file_out ( "primes.txt" );
+	size_t count = 0;
+	
+	std::ofstream file_out ( out_path );
 	for ( size_t cc = 1; cc <= chunk_to_elaborate; cc++ ) {
 		std::cout << "inizio ciclo: " << cc << '\n';
 
@@ -19,19 +24,29 @@ int main() {
 
 		// per tutti i numero da 2 alla radice dell'ultimo numero del chunk
 		for ( int i = 2; i < iterazioni; i++ ) {
-			std::cout << "iterazione: " << i << '\n';
+			// std::cout << "iterazione: " << i << '\n';
 
 			// per tutti i multipli di i a partire a i^2 fino all'ultimo numero del chunk
-			for ( int j = i * i; j < chunk_size + delta_chunk + 1; j+=i ) {
+			for ( int j = i * i; j < chunk_size + delta_chunk + 3; j+=i ) {
 				if ( j > delta_chunk ) {
-					primes[j - ( delta_chunk ) ] = 1;
+					primes[j - ( delta_chunk + 2 ) ] = 1;
 				}
 			}
 		}
 		
 		// printing
 		std::cout << "stampo ciclo: " << cc << '\n';
-		for (int i = 0; i < chunk_size; i++) { if (primes[i] == 0) { file_out << i + delta_chunk << '\t'; } }
+		for (int i = 0; i < chunk_size; i++) { 
+			if (primes[i] == 0) {
+				size_t out_n = delta_chunk + i + 2;
+				char* out_s = (char*)&out_n;
+				file_out.write ( out_s, 8);
+				std::cout << count << ": " << out_n << "\n";
+				count++;
+			} 
+		}
 		std::cout << "fine ciclo: " << cc << '\n';
 	}
+
+	std::cout << "trovati " << count << '\n'; 
 }
